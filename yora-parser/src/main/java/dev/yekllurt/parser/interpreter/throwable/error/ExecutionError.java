5bf6c3fcea7f54e91022d8a2f8b1260bbbafba5d
Steven Zwickel <e11907102@student.tuwien.ac.at>
2023-06-08 14:07:44 +0200

    Implement interpreter features
    * add: errors that can occur during execution of the program

---
+++ yora-parser/src/main/java/dev/yekllurt/parser/interpreter/throwable/error/ExecutionError.java
@@ -0,0 +1,9 @@
+package dev.yekllurt.parser.interpreter.throwable.error;
+
+public class ExecutionError extends Error {
+
+    public ExecutionError(String message) {
+        super(message);
+    }
+
+}
---
+++ yora-parser/src/main/java/dev/yekllurt/parser/interpreter/throwable/error/InvalidOperationError.java
@@ -0,0 +1,9 @@
+package dev.yekllurt.parser.interpreter.throwable.error;
+
+public class InvalidOperationError extends Error {
+
+    public InvalidOperationError(String message) {
+        super(message);
+    }
+
+}
