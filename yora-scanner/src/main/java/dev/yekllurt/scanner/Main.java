5f8254949ae0940984acb0e291d1f7d957bc8eae
Steven Zwickel <e11907102@student.tuwien.ac.at>
2023-06-19 15:30:56 +0200

    add: support for using the scanner via the console

--- yora-scanner/src/main/java/dev/yekllurt/scanner/Main.java
+++ yora-scanner/src/main/java/dev/yekllurt/scanner/Main.java
@@ -1,22 +1,40 @@
 package dev.yekllurt.scanner;
 
-import dev.yekllurt.scanner.token.Token;
-import dev.yekllurt.scanner.token.TokenDefinition;
 import dev.yekllurt.scanner.token.TokenDefinitionLoader;
 import dev.yekllurt.scanner.utility.FileUtility;
 
+import java.io.BufferedWriter;
 import java.io.File;
-import java.util.List;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Objects;
 
 public class Main {
 
-    // Creates a list of tokens
-    public static void main(String[] args) {
-        TokenDefinitionLoader tokenDefinitionLoader = new TokenDefinitionLoader();
-        List<TokenDefinition> tokenDefinitionList = tokenDefinitionLoader.load(new File("./examples/scanner/token-definition.yora-scanner"));
-        Scanner scanner = new Scanner(FileUtility.readFile(new File("./examples/test-program.yora")), tokenDefinitionList);
-        List<Token> tokenList = scanner.tokenize();
-        tokenList.forEach(token -> System.out.printf("%s %s%n", token.getType(), token.getValue()));
+    public static void main(String[] args) throws IOException {
+
+        if (!(args.length == 2 || args.length == 3)) {
+            System.out.println("Invalid argument length. Program arguments = <token-definition-file> <program-file> <token-output-file>");
+            System.exit(1);
+        }
+
+        var tokenDefinitionFile = args[0];
+        var programFile = args[1];
+        var tokenOutputFile = args.length == 3 ? args[2] : null;
+
+        var tokenDefinitionList = new TokenDefinitionLoader().load(new File(tokenDefinitionFile));
+        var scanner = new Scanner(FileUtility.readFile(new File(programFile)), tokenDefinitionList);
+        var tokens = scanner.tokenize();
+
+        if (Objects.nonNull(tokenOutputFile)) {
+            try (var writer = new BufferedWriter(new FileWriter(tokenOutputFile))) {
+                for (var token : tokens) {
+                    writer.write(String.format("%s %s%n", token.getType(), token.getValue()));
+                }
+            }
+        } else {
+            tokens.forEach(token -> System.out.printf("%s %s%n", token.getType(), token.getValue()));
+        }
     }
 
 }
